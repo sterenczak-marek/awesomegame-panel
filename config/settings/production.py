91687e5bac8fd205c926822285a3281bedbf66da
DjVidiX <djvidix.ms@gmail.com>
2017-01-13 00:21:08 +0100

    dodanie do allowed_hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -9,3 +9,7 @@ DATABASES = {
     'default': env.db(), # Raises ImproperlyConfigured exception if DATABASE_URL not in os.environ
     'extra': env.db('SQLITE_URL', default='sqlite:////tmp/my-tmp-sqlite.db')
 }
+
+ALLOWED_HOSTS = [
+    'awesomegame-panel.herokuapp.com',
+]
